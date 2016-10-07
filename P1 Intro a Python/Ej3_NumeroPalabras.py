6045c083f0bd90787d7bc4b91d3181c11604a89c
elielr01 <eli.elr01@gmail.com>
2016-10-07 15:05:10 +0200

    Ejercicio 3 terminado

--- P1 Intro a Python/Ej3_NumeroPalabras.py
+++ P1 Intro a Python/Ej3_NumeroPalabras.py
@@ -15,3 +15,38 @@
 
 
 # Ejercicio 3
+
+def contarPalabrasDeArchivo():
+    nombreArchivoEntrada = raw_input("Introduzca el nombre del archivo que quiere leer:\n")
+
+    # Se intenta abrir el fichero
+    try:
+        manf = open(nombreArchivoEntrada)
+    except:
+        print "No se ha podido abrir el archivo: ", nombreArchivoEntrada
+        exit()
+
+    # Si el fichero es correcto, se lee.
+    dicPalabras = dict()    # Este diccionario guardará las palabras y la cantidad de veces que aparece
+    for linea in manf:
+        linea = linea.rstrip()
+
+        lststrPalabras = linea.split()
+        for i in range(0, len(lststrPalabras)):
+            # Si la palabra ya exste en el diccionario, se le suma 1 ocurrencia
+            if lststrPalabras[i] in dicPalabras:
+                dicPalabras[lststrPalabras[i]] = dicPalabras[lststrPalabras[i]] + 1
+            # Si no, se agrega al diccionario
+            else:
+                dicPalabras[lststrPalabras[i]] = 1
+
+    # Se escribe las palabras y sus ocurrencias en un fichero de salida.
+    fsal = open('palabras.txt', 'w')
+
+    for clave in dicPalabras:
+        fsal.write(str(clave) + ' ' + str(dicPalabras[clave]) + '\n')
+
+    fsal.close()
+    manf.close()
+
+contarPalabrasDeArchivo()
