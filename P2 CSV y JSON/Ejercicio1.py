6571d8b9aa3740388c2dfa4b99ac6cfebe6e01a1
elielr01 <eli.elr01@gmail.com>
2016-10-26 16:16:06 +0200

    Ejercicio 2 comentado y terminado

--- P2 CSV y JSON/Ejercicio1.py
+++ P2 CSV y JSON/Ejercicio1.py
@@ -1,13 +1,12 @@
 import csv
 
-
 def aguaControlada():
-    
+
     #abrimos archivo
     archivoAgua = open("agua_eprtr_2008_030412.csv")
     lectorAgua = csv.reader(archivoAgua, delimiter=";")
     diccionario = dict()
-    
+
     #recorremos el lector
     for linea in lectorAgua:
         
@@ -15,17 +14,17 @@ def aguaControlada():
         if lectorAgua.line_num > 2:
             linea8 = str(linea[8])
             linea2 = str(linea[2])
-            
+
             #dependiendo si esta o no en el diccionario
-            if  linea8 in diccionario:   
+            if  linea8 in diccionario:
                 diccionario[linea8].append(linea2)
             else:
                 diccionario[linea8] = [linea2]
 
-    #guardamos en el otro archivo lo almacenado en el diccionario           
+    #guardamos en el otro archivo lo almacenado en el diccionario
     archivoWrite = open("AguaAgrupada.csv", "w")
     salidaEscrito = csv.writer(archivoWrite)
-    
+
     for linea in diccionario:
         for lin in diccionario[linea]:
             salidaEscrito.writerow([ linea + ',' + lin ])
@@ -40,11 +39,11 @@ def numFrecuencia():
     archivoRes = open("residuos_peligrosos_eprtr_2008_040412.csv")
     lectorRes = csv.reader(archivoRes, delimiter=";")
     diccionario = dict()
-    for linea in lectorRes:      
+    for linea in lectorRes:
         if lectorRes.line_num > 2:
             linea2 = str(linea[2])
             #vamos recopilando las veces que aparece
-            if linea2 in diccionario:   
+            if linea2 in diccionario:
                 diccionario[linea2] += 1
             else:
                 diccionario[linea2] = 1
@@ -64,7 +63,7 @@ def infoEmpresas():
     archivoRes = open("aire_eprtr_2008_030412.csv")
     lectorRes = csv.reader(archivoRes, delimiter=";")
     diccionario = dict()
-    
+
     for linea in lectorRes:
         if lectorRes.line_num > 2:
             linea2 = str(linea[2])
@@ -72,27 +71,27 @@ def infoEmpresas():
             #convertimos la cantidad
             aux = linea[10].split(",")
             linea10 = ".".join(aux)
-        
+
             #vamos acumulando en el diccionario
-            if linea2 in diccionario:   
+            if linea2 in diccionario:
                 diccionario[linea2] += float(linea10)
             else:
                 diccionario[linea2] = float(linea10)
-        
+
     #abrimos el archivo CSV para escribir
     archivoWrite = open("Contaminantes.csv", "w")
     salidaEscrito = csv.writer(archivoWrite)
-    
+
     # ordenamos la lista
     lista = sorted(diccionario.items(), key=lambda x:x[1])
-    
+
     #recorremos la lista y vamos guardando
     i = 0
     for linea in lista:
         if(len(lista) - i <= 10):
             salidaEscrito.writerow(lista[i])
         i += 1
-             
+
     archivoWrite.close()
 
 aguaControlada()
--- P2 CSV y JSON/Ejercicio2.py
+++ P2 CSV y JSON/Ejercicio2.py
@@ -1,81 +1,85 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import json
-import operator
 
 def Semaforo():
 
-    #Abrimos los archivos
+    # se abre el archivo que contiene el json
     try:
         archivo = open("semaforos.txt")
     except:
         print "No se ha podido abrir el fichero", "semaforos.txt"
         exit()
 
-
-    #leemos el json
+    # se lee el json
     aux = str(archivo.read())
     lista = json.loads(aux)
-
-    totalSemaforos = 0
 
     dicSemaforos = dict()
 
     for i in range(len(lista["features"])):
+
+        # se obtiene el xml en string de los atributos del semáforo
         cadena = lista["features"][i]["properties"]["Description"]
         str_cadena = cadena.encode("utf-8")
 
+        # se obtiene la ubicación del semáforo
         ubicacion_incompleta = str_cadena[str_cadena.find("Ubicación:</b>") +
             len("Ubicación:</b>"):]
 
         ubicacion = ubicacion_incompleta[:ubicacion_incompleta.find("<br />")]
         ubicacion = ubicacion.lstrip().rstrip()
 
-        ocurrencias_incompleta =
-            ubicacion_incompleta[ubicacion_incompleta.find("Semáforos:</b>") +
+        # se obtiene el número de ocurrencias para esa instancia de semáforo
+        ocurrencias_incompleta = ubicacion_incompleta[ubicacion_incompleta.find("Semáforos:</b>") +
             len("Semáforos:</b>"):]
-        ocurrencias =
-            ocurrencias_incompleta[:ocurrencias_incompleta.find("<br />")]
+        ocurrencias = ocurrencias_incompleta[:ocurrencias_incompleta.find("<br />")]
         ocurrencias = ocurrencias.lstrip().rstrip()
 
+        # se convierte a un dato de tipo Int las ocurrencias
         if ocurrencias == "":
             ocurrencias = 0
         else:
             ocurrencias = int(ocurrencias)
 
+        # Si el semáforo ya fue registrado, se le suma a sus ocurrencias
+        # Si no está registrado, simplemente se agrega al diccionario.
         if ubicacion in dicSemaforos:
             dicSemaforos[ubicacion] += ocurrencias
         else:
             dicSemaforos[ubicacion] = ocurrencias
 
-
+    # Ahora se guarda la frecuencia de cada semáforo en un archivo.
     try:
         fichero_frecuencias = open('frecuenciaSemaforos.txt', 'w')
     except:
         print "No se ha podido generar el archivo de salida"
         exit()
 
+    # Se suma el número total de ocurrencias de todos los semáforos
     numTotalSemaforos = 0
     for clave in dicSemaforos:
         numTotalSemaforos += dicSemaforos[clave]
 
+    # Ya con el número total de ocurrencias, se imprimen las frecuencias
     for clave in dicSemaforos:
         fichero_frecuencias.write(clave + "\t\tFrecuencia: " +
             str(float(dicSemaforos[clave]) / float(numTotalSemaforos)) + "\n")
 
     fichero_frecuencias.close()
 
-    # Guardo los 10 semáforos con más ocurrencias
+    # Ahora se guardan los 10 semáforos con más frecuencias en otro archivo
     try:
         fsal2 = open('MayorFrecuenciaSemaforos.txt', 'w')
     except:
         print "No se ha podido generar el archivo de salida MayorFrecuencia"
 
+    # Se crea una lista de tuplas del diccionario, ordenando los semáforos
+    # a partir de su número de ocurrencias de mayor a menor.
     semaforos_ordenados = sorted(dicSemaforos.items(), key=lambda x:x[1])
     semaforos_ordenados = list(reversed(semaforos_ordenados))
 
-
+    # Se imprimen los primeros 10 de la lista de semáforos creada.
     for semaforo in semaforos_ordenados[:10]:
         fsal2.write(semaforo[0] + "\t\tOcurrencias: " + str(semaforo[1]) + "\n")
 
